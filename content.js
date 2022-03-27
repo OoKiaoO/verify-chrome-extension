c5bc03d85310f69820d275e985a9f904e0ac4041
Chiara Campalani <campalanichiara@gmail.com>
2022-03-27 18:28:02 +0900

    implementd unverified items highlight on page load

--- content.js
+++ content.js
@@ -14,19 +14,38 @@
         clearInterval(jsInitCheckTimer);
         console.log("WE MADE IT!");
 
+        // highlighting background of unscanned items on In progress orders
+        const unscannedItems = document.querySelectorAll("div.bg-warning.barcode-quantity-container");
+        console.log("unscannedItems", unscannedItems);
+        if (orderStatus.innerHTML === "In progress" || orderStatus.innerHTML === "Opened") {
+          unscannedItems.forEach((item) => {
+            item.classList.remove("bg-warning");
+            item.style.backgroundColor = "yellow";
+            item.classList.add("highlited-items");
+          })
+        }
+
+        // extracting array of order items and calculating sum of unscanned items
         const remainingItemsArray = [...itemsList].map((item) => parseInt(item.innerHTML));
         const remainingItemsCount = remainingItemsArray.reduce((a, b) => a + b);
 
-        console.log("remainingItemsCount", remainingItemsCount);
+        // event listener activating popup alerts on ship button click event
         const handleClickAlert = () => {
-          if (orderStatus.innerHTML === "In progress" && remainingItemsCount)
-            { alert("Some items in this order are not verified!");    
+          if (orderStatus.innerHTML === "In progress" && remainingItemsCount) {
+            alert("Some items in this order are not verified!");    
+          } else if (orderStatus.innerHTML === "In progress" && !remainingItemsCount) {
+            const highlitedItems = document.getElementsByClassName("highlited-items");
+            if (highlitedItems.length < 1) {
+              return
+            } else {
+              highlitedItems.forEach((item) => item.style.backgroundColor = "#FFFFFF" );
+            }
           } else if(orderStatus.innerHTML === "Opened"
           || orderStatus.innerHTML === "Not Started"
           || orderStatus.innerHTML === "Partially cleared") {
-            alert("Some items in this order are not verified!"); // Send POPUP alert on screen
-              chrome.runtime.sendMessage({popupAlert: "yes"}); // Send message to backround to reactivate page update listener
-              // TODO: create general loop with message receiving listener to catch lost messages & avoid console errors.
+            alert("Some items in this order are not verified!");
+            chrome.runtime.sendMessage({popupAlert: "yes"}); // Send message to backround to reactivate page update listener
+            // TODO: create loop with message receiving listener to catch lost messages & avoid console errors.
           } else {
             chrome.runtime.sendMessage({popupAlert: "no"});
           }
